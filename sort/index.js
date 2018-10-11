cc4cb9a254c3f3fec181d4f017d910034a5f366e
htoopyae <htoopyae5@gmail.com>
2018-10-12 01:19:45 +0630

    Insertion Sort Added

--- sort/index.js
+++ sort/index.js
@@ -1,5 +1,7 @@
 const BubbleSort = require('./bubble_sort');
 const SelectionSort = require('./selection_sort');
+const InsertionSort = require('./insertion_sort');
 
 console.log(BubbleSort([5,2,3,1,10,7,0,9]));
 console.log(SelectionSort([5,2,3,1,10,7,0,9]));
+console.log(InsertionSort([5,2,3,1,10,7,0,9]));
---
+++ sort/insertion_sort.js
@@ -0,0 +1,20 @@
+/**
+ * @param   {Array} Array   unsorted
+ * @returns {Array}         sorted
+ */
+const InsertionSort = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        let j = i - 1;
+        const key = arr[i];
+
+        while (j >= 0 && (key < arr[j])) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+
+module.exports = InsertionSort;
