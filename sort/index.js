bf290adc3afe91e26062793bd912e5bc566782f5
htoopyae <htoopyae5@gmail.com>
2018-10-13 13:34:05 +0630

    Merge Sort Added

--- sort/index.js
+++ sort/index.js
@@ -1,7 +1,12 @@
 const BubbleSort = require('./bubble_sort');
 const SelectionSort = require('./selection_sort');
 const InsertionSort = require('./insertion_sort');
+const MergeSort = require('./merge_sort');
 
-console.log(BubbleSort([5,2,3,1,10,7,0,9]));
-console.log(SelectionSort([5,2,3,1,10,7,0,9]));
-console.log(InsertionSort([5,2,3,1,10,7,0,9]));
+let unsorted = [5,2,3,1,10,7,0,9];
+console.log("Unsorted Array : " + unsorted);
+
+console.log("Bubble Sort    : " + BubbleSort(unsorted));
+console.log("Selection Sort : " + SelectionSort(unsorted));
+console.log("Insertion Sort : " + InsertionSort(unsorted));
+console.log("Merge Sort     : " + MergeSort(unsorted));
---
+++ sort/merge_sort.js
@@ -0,0 +1,38 @@
+/**
+ * @param   {Array} arr unsorted
+ * @returns {Array}     sorted
+ */
+const merge = (left, right) => {
+    let i = 0, j = 0;
+    let partial_sorted = [];
+
+    while (i < left.length) {
+        if (right[j] < left[i]) {
+            partial_sorted.push(right[j++]);
+        }
+        else {
+            partial_sorted.push(left[i++]);
+        }
+    }
+
+    return partial_sorted.concat((i < left.length ? left.slice(i) : right.slice(j)));
+};
+
+const MergeSort = (arr) => {
+    let sorted;
+    
+    if (arr.length === 1) {
+        return arr;
+    }
+    else {
+        let mid = arr.length >> 1;
+        let left = MergeSort(arr.slice(0, mid));
+        let right = MergeSort(arr.slice(mid));
+
+        sorted = merge(left, right);
+    }
+
+    return sorted;
+};
+
+module.exports = MergeSort;
